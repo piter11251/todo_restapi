f622042dffc9ce0802970b674e622693db860865
piter11251 <duszeq13@gmail.com>
2024-12-01 17:18:44 +0100

    done

--- TodoAPI/TodoAPI/obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
+++ TodoAPI/TodoAPI/obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44dce25fefd4cdeb1793b08ca2cc09e6a65de4ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d192b8359f82fc50d1757bf61f259484c417309")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
